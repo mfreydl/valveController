db49fb018b3faf3a2692c3c53897154ee3ae0fa6
mfreydl <letmeaskmydog@gmail.com>
2017-10-14 20:27:38 -0400

    Configured a blinking LED so something fucking works to begin with.

--- src/vlvctl/vlvctl.ino
+++ src/vlvctl/vlvctl.ino
@@ -1,9 +1,20 @@
+
+
+int _ledPin = 13;
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(_ledPin, OUTPUT);
 
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  digitalWrite(_ledPin, HIGH);
+  delay(1000);
+  digitalWrite(_ledPin, LOW);
+  delay(1000);  
 }
